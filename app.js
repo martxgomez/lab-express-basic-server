1e821eb57aa950f31a635f2d9001381ac4837e2f
martxgomez <martagomezmartinez95@gmail.com>
2025-01-18 12:50:56 +0100

    Iteration 3 done

--- app.js
+++ app.js
@@ -16,15 +16,23 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
 // ROUTES
 // Start defining your routes here:
+app.get("/", (req, res)=>{
+    console.log("Esta es la request de GET /");
+    res.sendFile(__dirname+"/views/home.html")
+    }
+)
+
+app.get("/blog")
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
 
 const PORT = 5005;
 app.listen(PORT, () => {
-  console.log("Listening on Port 5005");
+    console.log("Listening on Port 5005");
+    console.log(__dirname);
 });
